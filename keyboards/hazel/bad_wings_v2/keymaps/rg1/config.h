441eb850dfda7ce90c5603161732f0c6323a047b
Richard Gaywood <commits@fscked.co.uk>
2025-04-29 20:20:52 +0100

    tweak some settings

--- keyboards/hazel/bad_wings_v2/keymaps/rg1/config.h
+++ keyboards/hazel/bad_wings_v2/keymaps/rg1/config.h
@@ -11,7 +11,21 @@
 #define ONESHOT_TAP_TOGGLE 10
 #define ONESHOT_TIMEOUT 500
 #define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
-#define CIRQUE_PINNACLE_TAP_ENABLE
+
+// cirque settings
+// https://docs.qmk.fm/features/pointing_device
 #define CIRQUE_PINNACLE_POSITION_MODE  CIRQUE_PINNACLE_ABSOLUTE_MODE
 
+// Touch originating in outer ring can trigger scroll by moving along
+// the perimeter. Near side triggers vertical scroll and far side
+// triggers horizontal scroll.
 #define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
+
+#define CIRQUE_PINNACLE_DIAMETER_MM 35
+
+// CIRQUE_PINNACLE_ATTENUATION
+// lowest sensitivity is default - EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_4X
+// also 1X, 2X, 3X
+
+// enable or disable taps
+#define CIRQUE_PINNACLE_TAP_ENABLE
--- keyboards/hazel/bad_wings_v2/keymaps/rg1/rules.mk
+++ keyboards/hazel/bad_wings_v2/keymaps/rg1/rules.mk
@@ -1,2 +1,9 @@
 MOUSEKEY_ENABLE			= yes
 EXTRAKEY_ENABLE			= yes
+
+
+# cirque settings - setings inherited from keyboard
+# POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
+# POINTING_DEVICE_ENABLE  = yes
+
+
