1cd2759df11a3885ffd7cbcc626e011d2fbf16fa
Richard Gaywood <commits@fscked.co.uk>
2025-05-17 17:46:47 +0100

    tame pointer speeds; implement auto mouse, drag scroll, pseudo layer lock

--- keyboards/hazel/bad_wings_v2/keymaps/rg1/keymap.c
+++ keyboards/hazel/bad_wings_v2/keymaps/rg1/keymap.c
@@ -1,12 +1,29 @@
-#include "pointing_device/pointing_device_auto_mouse.h"
 #include QMK_KEYBOARD_H
+
 #include "keymap_uk.h"
 
+#include "pointing_device/pointing_device_auto_mouse.h"
+// #include "features/layer_lock.h"
 
-#define CIRQUE_CPI_MIN 400
-#define CIRQUE_CPI_MAX 1200
-#define CIRQUE_CPI_STEP 100
+// #define CIRQUE_CPI_MIN 400
+// #define CIRQUE_CPI_MAX 1200
+// #define CIRQUE_CPI_STEP 100
 
+enum custom_keycodes {
+    DRAG_SCROLL = SAFE_RANGE,
+};
+
+#define CIRQUE_CPI 300
+
+// Modify these values to adjust the mouse drag_scroll speed
+#define SCROLL_DIVISOR_H 32.0
+#define SCROLL_DIVISOR_V 32.0
+
+// Variables to store accumulated scroll values
+float scroll_accumulated_h = 0;
+float scroll_accumulated_v = 0;
+// store state of drag scroll as the key is pressed and released
+bool set_scrolling = false;
 
 enum bad_wings_layers {
     QWERTY,
@@ -14,7 +31,7 @@ enum bad_wings_layers {
     SYMBOLS,
     ADJUST,
     NUMPAD,
-    MOUSE,
+    AUTO_MOUSE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -60,23 +77,69 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
-    [MOUSE] = LAYOUT_split_3x5_3(
-        KC_NO, KC_NO, KC_NO, KC_NO, QK_MOUSE_WHEEL_UP,                                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+    [AUTO_MOUSE] = LAYOUT_split_3x5_3(
+        KC_NO, KC_NO, KC_NO, KC_NO, QK_MOUSE_WHEEL_UP,                                  QK_MOUSE_WHEEL_UP, KC_NO, KC_NO, KC_NO, KC_NO,
         KC_NO, OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), OSM(MOD_LSFT),              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, QK_MOUSE_WHEEL_DOWN,                                KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, QK_MOUSE_WHEEL_DOWN,                                QK_MOUSE_WHEEL_DOWN, KC_NO, KC_NO, KC_NO, KC_NO,
         // thumbs
-        KC_TRNS, MS_BTN1, MS_BTN2,                                                      KC_TRNS, KC_TRNS, KC_TRNS
+        DRAG_SCROLL, MS_BTN1, MS_BTN2,                                                      KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 };
 
-
-
-
 void pointing_device_init_user(void) {
-    set_auto_mouse_layer(MOUSE);
     set_auto_mouse_enable(true);
-
-    pointing_device_set_cpi(350);
+    set_auto_mouse_layer(AUTO_MOUSE);
+    pointing_device_set_cpi(CIRQUE_CPI);
 };
 
+// Function to handle mouse reports and perform drag scrolling
+// code heavily cribbed from https://docs.qmk.fm/features/pointing_device#advanced-drag-scroll
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    // Check if drag scrolling is active
+    if (set_scrolling) {
+        // Calculate and accumulate scroll values based on mouse movement and divisors
+        scroll_accumulated_h += (float)mouse_report.x / SCROLL_DIVISOR_H;
+        scroll_accumulated_v += (float)mouse_report.y / SCROLL_DIVISOR_V;
+
+        // Assign integer parts of accumulated scroll values to the mouse report
+        mouse_report.h = (int8_t)scroll_accumulated_h;
+        mouse_report.v = (int8_t)scroll_accumulated_v;
+
+        // Update accumulated scroll values by subtracting the integer parts
+        scroll_accumulated_h -= (int8_t)scroll_accumulated_h;
+        scroll_accumulated_v -= (int8_t)scroll_accumulated_v;
+
+        // Clear the X and Y values of the mouse report
+        mouse_report.x = 0;
+        mouse_report.y = 0;
+    }
+    return mouse_report;
+}
+
+// Function to handle key events and enable/disable drag scrolling
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case DRAG_SCROLL:
+            // Toggle set_scrolling when DRAG_SCROLL key is pressed or released
+            set_scrolling = record->event.pressed;
+            // lock the mouse layer on
+            layer_lock_on(AUTO_MOUSE);
+            set_auto_mouse_enable(false);
+            break;
+        default:
+            layer_lock_off(AUTO_MOUSE);
+            set_auto_mouse_enable(true);
+            break;
+    }
+    return true;
+}
+
+// Function to handle layer changes and disable drag scrolling when not in AUTO_MOUSE_DEFAULT_LAYER
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Disable set_scrolling if the current layer is not the AUTO_MOUSE_DEFAULT_LAYER
+    if (get_highest_layer(state) != AUTO_MOUSE) {
+        set_scrolling = false;
+    }
+    return state;
+}
--- keyboards/hazel/bad_wings_v2/keymaps/rg1/rules.mk
+++ keyboards/hazel/bad_wings_v2/keymaps/rg1/rules.mk
@@ -3,9 +3,9 @@ EXTRAKEY_ENABLE			= yes
 
 BOOTMAGIC_ENABLE = yes
 
+LAYER_LOCK_ENABLE = yes
 
 # cirque settings - setings inherited from keyboard
 # POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
 # POINTING_DEVICE_ENABLE  = yes
-
 
